89762ae4529a53fa5eb6692286f12802a8d01c80
Kabilan-R-S <kabilanrasi@gmail.com>
2025-02-25 14:06:15 +0530

    Delete Verilog/Day 2 directory

--- Verilog/Day 2/Half Adder/half_adder.v
+++
@@ -1,4 +0,0 @@
-module halfadder (input a, b, output c, s);
-    assign s = a ^ b;
-    assign c = a & b;
-endmodule
--- Verilog/Day 2/Half Subractor/half_subractor.v
+++
@@ -1,6 +0,0 @@
-module halfsubtractor(input a, b, output d, B);
-    wire w1;
-    assign d = a ^ b;
-    assign w1 = ~a;
-    assign B = w1 & b;
-endmodule
