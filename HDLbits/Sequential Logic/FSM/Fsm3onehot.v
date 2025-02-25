54d9d21441e2a56854a39f4b0eea06a753468d66
Kabilan-R-S <kabilanrasi@gmail.com>
2025-02-25 13:41:36 +0530

    Create Fsm3onehot.v

---
+++ HDLbits/Sequential Logic/FSM/Fsm3onehot.v
@@ -0,0 +1,20 @@
+module top_module(
+    input in,
+    input [3:0] state,
+    output [3:0] next_state,
+    output out); //
+
+    parameter A=0, B=1, C=2, D=3;
+
+    // State transition logic: Derive an equation for each state flip-flop.
+    assign next_state[A] = state[A]&(~in) | state[C]&(~in);
+    assign next_state[B] = state[B]&in | state[D]&in | state[A]&in;
+    assign next_state[C] = state[B]&(~in) | state[D]&(~in);
+    assign next_state[D] = state[C]&in;
+
+    // Output logic: 
+    assign out = (state[D]==1);
+
+
+//one hot is quite complicated but it's easy to understanding , simply what are all state comes under their states you need to represent them.
+endmodule
